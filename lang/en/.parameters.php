51e57d9c891bc4c603bbd44c7fc0c50a4d1b1b03
Konstantin <kvt@iplogic.ru>
2022-01-11 12:57:48 +0300

    Language files fix

--- lang/en/.parameters.php
+++ lang/en/.parameters.php
@@ -1,6 +1,6 @@
 <?
 $MESS['PARAMETER_SHOW_ERRORS'] = "Show errors";
-$MESS['PARAMETER_CHANNEL_ID'] = "Chanel ID";
+$MESS['PARAMETER_CHANNEL_ID'] = "Channel ID";
 $MESS['PARAMETER_NUM'] = "Number of posts";
 $MESS['PARAMETER_PREVIEW_FILENAME'] = "The name of the preview file on YouTube";
 ?>
